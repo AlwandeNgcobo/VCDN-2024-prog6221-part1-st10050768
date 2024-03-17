d7450b05d346377829febd7e8c73464903420323
Alwande Ngcobo <alwandengcobo3@gmail.com>
2024-03-17 19:41:36 +0200

    Variable declaration completed and name of recipe along with number of ingredients completed

---
+++ ST10050768_RecipeApp_PROG6221/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ST10050768_RecipeApp_PROG6221
+{
+    public class Program
+    {
+        static string? recipeDetails;
+        static int noOfIngredients;
+        static string? name;
+        static List<string> ingredientName =new List<string>();
+        static string? noOfSteps;
+        static List<string>? recipeSteps;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Recipe");
+            recipeDetails = Console.ReadLine();
+            Console.WriteLine("Enter number of ingredients");
+            noOfIngredients = int.Parse(Console.ReadLine());
+            for (int i = 0; i < noOfIngredients; i++)
+            {
+                Console.WriteLine("Enter name of ingredient");
+                name = Console.ReadLine();
+                ingredientName.Add(name);
+            }
+            new Recipe(recipeDetails, ingredientName);
+            Console.WriteLine(new Recipe(recipeDetails, ingredientName).toString());
+        }
+    }
+}
---
+++ ST10050768_RecipeApp_PROG6221/Recipe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10050768_RecipeApp_PROG6221
+{
+    public class Recipe
+    {
+        private string recipeDetails { get; set; }
+        private List<string> ingredientName { get; set; }
+        private List<string> ingredientQuantity { get; set; }
+        private List<string> ingredientUnitOfMeasurement { get; set; }
+        private List<string> recipeSteps { get; set; }
+
+        public Recipe(string recipeDetails, List<string> ingredientName)
+        {
+            this.recipeDetails = recipeDetails;
+            this.ingredientName = ingredientName;
+            
+        }
+        public string toString()
+        {
+            return "recipe: "+recipeDetails+"" +
+                "\nIngredient name: "+ string.Join(", ", ingredientName); ;
+        }
+    }
+}
