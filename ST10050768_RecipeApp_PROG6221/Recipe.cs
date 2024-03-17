51fd2ac22e6bb4c1b143521b997ddb51982c17d2
Alwande Ngcobo <alwandengcobo3@gmail.com>
2024-03-17 20:00:31 +0200

    Recipe Class created
    Recipe class created containing private variable declarqations and a constructor that will store the recipe information although so far it only stores the recipe name and name of ingredients.

--- ST10050768_RecipeApp_PROG6221/Recipe.cs
+++ ST10050768_RecipeApp_PROG6221/Recipe.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +14,7 @@ namespace ST10050768_RecipeApp_PROG6221
         private List<string> ingredientUnitOfMeasurement { get; set; }
         private List<string> recipeSteps { get; set; }
 
-        public Recipe(string recipeDetails, List<string> ingredientName)
+        public Recipe(string recipeDetails, List<string> ingredientName, List<string> ingredientQuantity, List<string> ingredientUnitOfMeasurement, List<string> recipesteps)
         {
             this.recipeDetails = recipeDetails;
             this.ingredientName = ingredientName;
@@ -23,8 +22,9 @@ namespace ST10050768_RecipeApp_PROG6221
         }
         public string toString()
         {
-            return "recipe: "+recipeDetails+"" +
-                "\nIngredient name: "+ string.Join(", ", ingredientName); ;
+            return "recipe: " + recipeDetails + "" +
+                "\nIngredient name: " + string.Join(", ", ingredientName);
+            //string.Join(", ", ingredientName) from https://stackoverflow.com/questions/16106181/why-am-i-getting-system-collections-generic-list1system-string-instead-of-the
         }
     }
 }
